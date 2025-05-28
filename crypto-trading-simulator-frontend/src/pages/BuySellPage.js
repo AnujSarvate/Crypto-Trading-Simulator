47406489f9b38e536bd9defdeeaebb449bd536b3
anuj <anujsarvate11@gmail.com>
2025-05-28 17:57:10 -0400

    Fixes

--- crypto-trading-simulator-frontend/src/pages/BuySellPage.js
+++ crypto-trading-simulator-frontend/src/pages/BuySellPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from '../utils/axiosInstance';
 import BuySellForm from '../components/BuySellForm';
 
